d6ca24fc392d95920f7abbe3c4317cd0aa2c2553
김태환 <taehwankim@gimtaehwan-ui-MacBookAir.local>
2022-08-22 03:51:29 +0900

    ADD useState method

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import logo from './logo.svg';
-import './App.css';
+// import logo from "./logo.svg";
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  let logo = "ReactBlog";
+  let [title, a] = useState([
+    "남자 코트 추천",
+    "여자 코트 추천",
+    "공용 코트 추천",
+  ]);
+  let [likes, b] = useState(0);
+
   return (
     <div className="App">
-      <h1>Hello, This is the first commit on -P-Blog_With_React</h1>
+      <div className="black-nav">
+        <h4>{logo}</h4>
+      </div>
+      <div className="list">
+        <h4>{title[0]} <span>👍</span> {likes} </h4>
+        <p>8월 19일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[1]}</h4>
+        <p>8월 20일 발행</p>
+      </div>
+      <div className="list">
+        <h4>{title[2]}</h4>
+        <p>8월 22일 발행</p>
+      </div>
     </div>
   );
 }
