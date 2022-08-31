c19f987665751002b2f884548362a0d8d3904e4a
김태환 <monte6198@gmail.com>
2022-09-01 05:18:24 +0900

    ADD data

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import "./App.css";
 import { useState } from "react";
-import a from "./data";
+import data from "./data";
 import { Container, Navbar, Nav } from "react-bootstrap";
 
 function App() {
+  let [crayons] = useState(data);
+
   return (
     <div className="App">
       <Navbar bg="light" varian="light">
@@ -18,30 +20,15 @@ function App() {
 
       <div className="main-bg container">
         <div className="row">
-          <div className="col-md-4">
-            <img
-              height="300px"
-              src="https://www.ikea.com/kr/en/images/products/mala-wax-crayon-mixed-colours__1089237_pe861528_s5.jpg?f=s"
-            />
-            <h4>Title</h4>
-            <p>Details</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              height="300px"
-              src="https://cdn11.bigcommerce.com/s-3stx4pub31/images/stencil/1280x1280/products/5543/14950/crayola-crayons-24units-colors__83433.1650556731.jpg?c=2?imbypass=on"
-            />
-            <h4>Title</h4>
-            <p>Details</p>
-          </div>
-          <div className="col-md-4">
-            <img
-              height="300px"
-              src="https://www.consumer.go.kr/site/consumer/upload/recall/RCLL_000000000013104_20180522023002482.png"
-            />
-            <h4>Title</h4>
-            <p>Details</p>
-          </div>
+          {crayons.map(function (a, i) {
+            return (
+              <div className="col-md-4">
+                <img height="300px" src={crayons[i].image} />
+                <h4>{crayons[i].Company}</h4>
+                <p>{crayons[i].price}</p>
+              </div>
+            );
+          })}
         </div>
       </div>
     </div>
--- src/data.js
+++ src/data.js
@@ -1,3 +1,26 @@
-let a = 10;
+let data = [
+  {
+    id: 0,
+    Company: "MALA",
+    content: "Mala Crayon, magnificent soft upscale crayon",
+    price: 3000,
+    image: "https://www.ikea.com/kr/en/images/products/mala-wax-crayon-mixed-colours__1089237_pe861528_s5.jpg?f=s",
+  },
+  {
+    id: 1,
+    Company: "Crayola",
+    content: "Crayola Crayon, the biggest crayon company",
+    price: 5000,
+    image:
+      "https://cdn11.bigcommerce.com/s-3stx4pub31/images/stencil/1280x1280/products/5543/14950/crayola-crayons-24units-colors__83433.1650556731.jpg?c=2?imbypass=on",
+  },
+  {
+    id: 2,
+    Company: "Colorations",
+    content: "Coloration Crayon, extra-large but cheap",
+    price: 3000,
+    image: "https://www.consumer.go.kr/site/consumer/upload/recall/RCLL_000000000013104_20180522023002482.png",
+  },
+];
 
-export default a;
+export default data;
