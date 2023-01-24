814e71f5b4e0dad6d125c883168fbae4e2cf7b2d
Monte <monte6198@gmail.com>
2023-01-25 06:16:04 +0900

    feat: api로부터 데이터 가져오기

--- react/monsters-rolodex/src/App.js
+++ react/monsters-rolodex/src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import { Component } from 'react';
+
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      monsters: [],
+    };
+  }
+
+  componentDidMount() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+      .then((response) => response.json())
+      .then((users) =>
+        this.setState(
+          () => {
+            return { monsters: users };
+          },
+          () => {
+            console.log(this.state);
+          },
+        ),
+      );
+    // .then((response) => {
+    //   response.json()
+    // })
+    // .then((users) => {console.log(users)});
+    // 중괄호 안에 response.json() 과 밖에 있는 것의 결과가 왜 다른가?
+    // -> 화살표 함수 확인
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+          // 리액트가 key를 요구하는 이유는 항상 re-render 할 때마다 배열 데이터 모두를 하지 않고 최적화된 방식으로, 바뀐 요소만을 바꾸기 위함.
+        })}
+      </div>
+    );
+  }
 }
 
 export default App;
