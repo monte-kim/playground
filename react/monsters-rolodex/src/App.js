ef2cfc664ef585b39b19982fc2ffa4ed26bc0992
Monte <monte6198@gmail.com>
2023-01-25 08:31:25 +0900

    feat: 몬스터 이름 검색 기능 추가

--- react/monsters-rolodex/src/App.js
+++ react/monsters-rolodex/src/App.js
@@ -4,16 +4,21 @@ import './App.css';
 
 class App extends Component {
   constructor() {
+    console.log('constructor');
     super();
     this.state = {
       monsters: [],
+      searchField: '',
     };
   }
 
   componentDidMount() {
+    console.log('componentDidMount');
     fetch('https://jsonplaceholder.typicode.com/users')
-      .then((response) => response.json())
-      .then((users) =>
+      .then((response) => {
+        return response.json();
+      })
+      .then((users) => {
         this.setState(
           () => {
             return { monsters: users };
@@ -21,20 +26,31 @@ class App extends Component {
           () => {
             console.log(this.state);
           },
-        ),
-      );
-    // .then((response) => {
-    //   response.json()
-    // })
-    // .then((users) => {console.log(users)});
-    // 중괄호 안에 response.json() 과 밖에 있는 것의 결과가 왜 다른가?
-    // -> 화살표 함수 확인
+        );
+      });
   }
 
   render() {
+    console.log('render');
+
+    const filteredMonsters = this.state.monsters.filter((monster) => {
+      return monster.name.toLocaleLowerCase().includes(this.state.searchField);
+    });
+
     return (
       <div className='App'>
-        {this.state.monsters.map((monster) => {
+        <input
+          className='search-box'
+          type='search'
+          placeholder='search monsters'
+          onChange={(event) => {
+            const searchField = event.target.value.toLocaleLowerCase();
+            this.setState(() => {
+              return { searchField };
+            });
+          }}
+        />
+        {filteredMonsters.map((monster) => {
           return (
             <div key={monster.id}>
               <h1>{monster.name}</h1>
