a350c2fc25f8e3dcea8a80335f909c0ff81a6806
Monte <monte6198@gmail.com>
2023-01-30 13:34:02 +0900

    refactor: 몬스터 카드 컴포넌트 분리

--- react/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ react/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,6 +1,7 @@
 import { Component } from 'react';
 
 import './card-list.styles.css';
+import Card from '../card/card.component';
 
 class CardList extends Component {
   render() {
@@ -10,18 +11,7 @@ class CardList extends Component {
     return (
       <div className='card-list'>
         {monsters.map((monster) => {
-          const { name, email, id } = monster;
-          return (
-            <div className='card-container' key={id}>
-              {/* // 리액트가 key를 요구하는 이유는 항상 re-render 할 때마다 배열 데이터 모두를 하지 않고 최적화된 방식으로, 바뀐 요소만을 바꾸기 위함. */}
-              <img
-                alt={`monster ${name}`}
-                src={`https://robohash.org/${id}?set=set2&size=180x180`}
-              />
-              <h2>{name}</h2>
-              <p>{email}</p>
-            </div>
-          );
+          return <Card monster={monster} />;
         })}
       </div>
     );
---
+++ react/monsters-rolodex/src/components/card/card.component.jsx
@@ -0,0 +1,20 @@
+import { Component } from 'react';
+
+import './card.styles.css';
+
+class Card extends Component {
+  render() {
+    const { name, email, id } = this.props.monster;
+
+    return (
+      <div className='card-container' key={id}>
+        {/* // 리액트가 key를 요구하는 이유는 항상 re-render 할 때마다 배열 데이터 모두를 하지 않고 최적화된 방식으로, 바뀐 요소만을 바꾸기 위함. */}
+        <img alt={`monster ${name}`} src={`https://robohash.org/${id}?set=set2&size=180x180`} />
+        <h2>{name}</h2>
+        <p>{email}</p>
+      </div>
+    );
+  }
+}
+
+export default Card;
