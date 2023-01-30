9a91b5bf04ef2f7a237e25edc7198b8a1eeaaa4d
Monte <monte6198@gmail.com>
2023-01-30 13:13:55 +0900

    feat: 몬스터 데이터 배치 추가

--- react/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ react/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -1,16 +1,27 @@
 import { Component } from 'react';
 
+import './card-list.styles.css';
+
 class CardList extends Component {
   render() {
-    console.log('render from CardList');
     // 리액트가 re-render 할 때는, state가 변할 때, 또는 props가 변할 때이다.
     const { monsters } = this.props;
 
     return (
-      <div>
+      <div className='card-list'>
         {monsters.map((monster) => {
-          return <h1 key={monster.id}>{monster.name}</h1>;
-          // 리액트가 key를 요구하는 이유는 항상 re-render 할 때마다 배열 데이터 모두를 하지 않고 최적화된 방식으로, 바뀐 요소만을 바꾸기 위함.
+          const { name, email, id } = monster;
+          return (
+            <div className='card-container' key={id}>
+              {/* // 리액트가 key를 요구하는 이유는 항상 re-render 할 때마다 배열 데이터 모두를 하지 않고 최적화된 방식으로, 바뀐 요소만을 바꾸기 위함. */}
+              <img
+                alt={`monster ${name}`}
+                src={`https://robohash.org/${id}?set=set2&size=180x180`}
+              />
+              <h2>{name}</h2>
+              <p>{email}</p>
+            </div>
+          );
         })}
       </div>
     );
