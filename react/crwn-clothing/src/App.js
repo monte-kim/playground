8e6ef43a844c0ad62f3a350d903e19809aff80c4
Monte <monte6198@gmail.com>
2023-02-01 15:53:22 +0900

    refactor: 카테고리 목록 컴포넌트 분리

--- react/crwn-clothing/src/App.js
+++ react/crwn-clothing/src/App.js
@@ -1,4 +1,5 @@
 import './categories.styles.scss';
+import CategoryItem from './components/category-item/category-item.component';
 
 const App = () => {
   const categories = [
@@ -32,20 +33,7 @@ const App = () => {
   return (
     <div className='categories-container'>
       {categories.map((category) => {
-        return (
-          <div className='category-container' key={category.id}>
-            <div
-              className='background-image'
-              style={{
-                backgroundImage: `url(${category.imageUrl})`,
-              }}
-            />
-            <div className='category-body-container'>
-              <h2>{category.title}</h2>
-              <p>Shop Now</p>
-            </div>
-          </div>
-        );
+        return <CategoryItem key={category.id} category={category} />;
       })}
     </div>
   );
