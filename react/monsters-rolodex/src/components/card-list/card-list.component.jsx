55695e0057590f3e7ec9ff29b5915988799fa518
Monte <monte6198@gmail.com>
2023-02-01 12:46:22 +0900

    feat: CardList key 추가

--- react/monsters-rolodex/src/components/card-list/card-list.component.jsx
+++ react/monsters-rolodex/src/components/card-list/card-list.component.jsx
@@ -10,7 +10,7 @@ const CardList = ({ monsters }) => {
   return (
     <div className='card-list'>
       {monsters.map((monster) => {
-        return <Card monster={monster} />;
+        return <Card monster={monster} key={monster.id} />;
       })}
     </div>
   );
