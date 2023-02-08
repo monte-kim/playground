f53c22a7fa48b01cb829bdbac45fcc062f7b9819
Monte <monte6198@gmail.com>
2023-02-08 23:24:16 +0900

    feat: 이메일 및 비밀번호 로그인 기능 추가

--- react/crwn-clothing/src/utils/firebase/firebase.utils.js
+++ react/crwn-clothing/src/utils/firebase/firebase.utils.js
@@ -9,6 +9,7 @@ import {
   signInWithPopup,
   GoogleAuthProvider,
   createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
 } from 'firebase/auth';
 
 import { getFirestore, doc, getDoc, setDoc } from 'firebase/firestore';
@@ -71,4 +72,9 @@ export const createUserDocumentFromAuth = async (userAuth, additionalInformation
 export const createAuthUserWithEmailAndPassword = async (email, password) => {
   if (!email || !password) return;
   return createUserWithEmailAndPassword(auth, email, password);
+};
+
+export const signInAuthUserWithEmailAndPassword = async (email, password) => {
+  if (!email || !password) return;
+  return signInWithEmailAndPassword(auth, email, password);
 };
