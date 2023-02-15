aa98cd4d78041be6cea6e419aaedb6cf658ea729
Monte <monte6198@gmail.com>
2023-02-15 23:21:57 +0900

    feat: Navigation Bar 추가

--- mern/simple-mern-project/frontend/src/App.js
+++ mern/simple-mern-project/frontend/src/App.js
@@ -1,14 +1,20 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
 import NewPlaces from './Places/pages/Places';
 import Users from './Users/pages/Users';
+import MainNavigation from './shared/UIElements/Navigation/MainNavigation';
 
 function App() {
   return (
-    <Routes>
-      <Route path='/' element={<Users />}></Route>
-      <Route path='/places/new' element={<NewPlaces />}></Route>
-      <Route path='*' element={<Navigate to='/' replace />} />
-    </Routes>
+    <>
+      <MainNavigation />
+      <main>
+        <Routes>
+          <Route path='/' element={<Users />}></Route>
+          <Route path='/places/new' element={<NewPlaces />}></Route>
+          <Route path='*' element={<Navigate to='/' replace />} />
+        </Routes>
+      </main>
+    </>
   );
 }
 
