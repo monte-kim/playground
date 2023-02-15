e83a2cb85facb7782df67e30459e353f714a8c42
Monte <monte6198@gmail.com>
2023-02-15 23:21:43 +0900

    feat: 사용자 목록 길이 0일 때 컴포넌트 스타일 수정

--- mern/simple-mern-project/frontend/src/Users/components/UserList/UsersList.js
+++ mern/simple-mern-project/frontend/src/Users/components/UserList/UsersList.js
@@ -1,4 +1,5 @@
 import UserItem from '../UserItem/UserItem';
+import Card from '../../../shared/UIElements/Card/Card';
 
 import './UsersList.scss';
 
@@ -6,7 +7,9 @@ const UsersList = (props) => {
   if (props.items.length === 0) {
     return (
       <div className='center'>
-        <h2>No users found.</h2>
+        <Card>
+          <h2>No users found.</h2>
+        </Card>
       </div>
     );
   }
