2410472f08d5ec5a0712ab7054e319fe2affbda6
Monte <monte6198@gmail.com>
2023-02-26 17:49:05 +0900

    feat: mongodb 연결

--- mern/realist/server/server.js
+++ mern/realist/server/server.js
@@ -1,8 +1,22 @@
 import express from 'express';
 import morgan from 'morgan';
 import cors from 'cors';
+import mongoose from 'mongoose';
+
+import { DATABASE } from './config.js';
 
 const app = express();
+
+// db
+mongoose.set('strictQuery', false);
+mongoose
+  .connect(DATABASE)
+  .then(() => {
+    console.log('mongodb_connected.');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 // middlewares
 app.use(express.json());
