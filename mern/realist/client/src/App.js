ea53797e558e63d6986bd386e16bfa379a8d44fc
Monte <monte6198@gmail.com>
2023-03-09 02:51:54 +0900

    feat: Navbar 추가

--- mern/realist/client/src/App.js
+++ mern/realist/client/src/App.js
@@ -1,16 +1,20 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
+import Main from './components/nav/Main';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 
 const App = () => {
   return (
-    <Routes>
-      <Route path='/' element={<Home />} />
-      <Route path='/login' element={<Login />} />
-      <Route path='/register' element={<Register />} />
-    </Routes>
+    <>
+      <Main />
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/register' element={<Register />} />
+      </Routes>
+    </>
   );
 };
 
