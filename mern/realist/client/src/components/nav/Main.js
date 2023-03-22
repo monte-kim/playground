91ade726a857b6609ef613f9e11c94c585ae606a
Monte <monte6198@gmail.com>
2023-03-22 15:51:26 +0900

    feat: 로그인 상태에 따른 NavBar 조건부 랜더링으로 수정

--- mern/realist/client/src/components/nav/Main.js
+++ mern/realist/client/src/components/nav/Main.js
@@ -15,37 +15,56 @@ const Main = () => {
     navigate('/login');
   };
 
+  // 로그인 및 회원가입 / 로그아웃 UI 조건무 렌더링
+  const loggedIn =
+    auth.user !== null && auth.token !== '' && auth.refreshToken !== '';
+
   return (
     <nav className='nav d-flex justify-content-between lead'>
       <NavLink className='nav-link' aria-current='page' to='/'>
         Home
       </NavLink>
-      <NavLink className='nav-link' to='/login'>
-        Login
-      </NavLink>
-      <NavLink className='nav-link' to='/register'>
-        Register
-      </NavLink>
 
-      <div className='dropdown'>
-        <li>
-          <a className='nav-link dropdown-toggle' data-bs-toggle='dropdown'>
-            User
-          </a>
-          <ul className='dropdown-menu'>
+      {/* 로그인 전이라면 로그인 및 회원가입 띄우기 */}
+      {loggedIn ? (
+        ''
+      ) : (
+        <>
+          <NavLink className='nav-link' to='/login'>
+            Login
+          </NavLink>
+          <NavLink className='nav-link' to='/register'>
+            Register
+          </NavLink>
+        </>
+      )}
+
+      {/* 로그인 상태라면 회원정보 드롭바 띄우기 */}
+      {loggedIn ? (
+        <>
+          <div className='dropdown'>
             <li>
-              <NavLink className='nav-link' to='/dashboard'>
-                Dashboard
-              </NavLink>
-            </li>
-            <li>
-              <a onClick={logout} className='nav-link'>
-                Logout
+              <a className='nav-link dropdown-toggle' data-bs-toggle='dropdown'>
+                {auth.user.name ? auth.user.name : auth.user.username}
               </a>
+              <ul className='dropdown-menu'>
+                <li>
+                  <NavLink className='nav-link' to='/dashboard'>
+                    Dashboard
+                  </NavLink>
+                </li>
+                <li>
+                  <a onClick={logout} className='nav-link'>
+                    Logout
+                  </a>
+                </li>
+              </ul>
             </li>
-          </ul>
-        </li>
-      </div>
+          </div>
+        </>
+      ) : (
+        ''
+      )}
     </nav>
   );
 };
