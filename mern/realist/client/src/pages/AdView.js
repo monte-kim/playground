4d30ad73247cd7a6a13925850ed3c7476908f4cd
Monte <monte6198@gmail.com>
2023-04-10 23:40:02 +0900

    feat: 서버로부터 사용자 Ad 가져오기 추가

--- mern/realist/client/src/pages/AdView.js
+++ mern/realist/client/src/pages/AdView.js
@@ -1,8 +1,33 @@
 import { useParams } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const AdView = () => {
+  // state
+  const [ad, setAd] = useState({});
+  const [related, setRelated] = useState([]);
+  // hooks
   const params = useParams();
-  return <>{params.slug}</>;
+
+  useEffect(() => {
+    if (params?.slug) fetchAd();
+  }, [params?.slug]);
+
+  const fetchAd = async () => {
+    try {
+      const { data } = await axios.get(`/ad/${params.slug}`);
+      setAd(data?.ad);
+      setRelated(data?.related);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <>
+      <pre>{JSON.stringify({ ad, related }, null, 4)}</pre>
+    </>
+  );
 };
 
 export default AdView;
