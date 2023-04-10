fdf6e36cf7c66588bde60091235c80a7b629ede8
Monte <monte6198@gmail.com>
2023-04-10 13:48:29 +0900

    feat: 서버로부터 Ads 가져오기

--- mern/realist/client/src/pages/Home.js
+++ mern/realist/client/src/pages/Home.js
@@ -1,12 +1,33 @@
+import { useEffect, useState } from 'react';
 import { useAuth } from '../context/auth';
+import axios from 'axios';
 
 const Home = () => {
+  // context
   const [auth, setAuth] = useAuth();
+
+  // state
+  const [adsForSell, setAdsForSell] = useState();
+  const [adsForRent, setAdsForRent] = useState();
+
+  useEffect(() => {
+    fetchAds();
+  }, []);
+
+  const fetchAds = async () => {
+    try {
+      const { data } = await axios.get('/ads');
+      setAdsForSell(data.adsForSell);
+      setAdsForRent(data.adsForRent);
+    } catch (err) {
+      console.log(err);
+    }
+  };
 
   return (
     <div>
       <h1 className='display-1 bg-primary text-light p-5'>Home</h1>
-      <pre>{JSON.stringify(auth, null, 4)}</pre>
+      <pre>{JSON.stringify({ adsForSell, adsForRent }, null, 4)}</pre>
     </div>
   );
 };
