d2b55cda7a648a5182916a6e5e7c6bb18c2578b2
Monte <monte6198@gmail.com>
2023-04-11 22:19:24 +0900

    feat: AdVeiw 이미지 확대 모달창 추가

--- mern/realist/client/src/pages/AdView.js
+++ mern/realist/client/src/pages/AdView.js
@@ -1,7 +1,8 @@
 import { useParams } from 'react-router-dom';
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useCallback } from 'react';
 import axios from 'axios';
 import Gallery from 'react-photo-gallery';
+import Carousel, { Modal, ModalGateway } from 'react-images';
 
 const photos = [
   {
@@ -20,6 +21,8 @@ const AdView = () => {
   // state
   const [ad, setAd] = useState({});
   const [related, setRelated] = useState([]);
+  const [current, setCurrent] = useState(0);
+  const [isOpen, setIsOpen] = useState(false);
   // hooks
   const params = useParams();
 
@@ -37,9 +40,33 @@ const AdView = () => {
     }
   };
 
+  const openLightBox = useCallback((event, { photo, index }) => {
+    setCurrent(index);
+    setIsOpen(true);
+  }, []);
+
+  const closeLightBox = () => {
+    setCurrent(0);
+    setIsOpen(false);
+  };
+
   return (
     <>
-      <Gallery photos={photos} />
+      <Gallery photos={photos} onClick={openLightBox} />
+      <ModalGateway>
+        {isOpen ? (
+          <Modal onClose={closeLightBox}>
+            <Carousel
+              currentIndex={current}
+              views={photos.map((x) => ({
+                ...x,
+                srcset: x.srcSet,
+                caption: x.title,
+              }))}
+            />
+          </Modal>
+        ) : null}
+      </ModalGateway>
       <pre>{JSON.stringify({ ad, related }, null, 4)}</pre>
     </>
   );
