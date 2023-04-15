88b5bfeecba038218033fac9d3c7d615ef36d5a2
Monte <monte6198@gmail.com>
2023-04-15 21:45:52 +0900

    feat: Profile 업데이트 페이지 추가

--- mern/realist/client/src/pages/user/Profile.js
+++ mern/realist/client/src/pages/user/Profile.js
@@ -1,12 +1,129 @@
+import { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+import toast from 'react-hot-toast';
+import slugify from 'slugify';
+
+import { useAuth } from '../../context/auth';
 import Sidebar from '../../components/nav/Sidebar';
 
 const Profile = () => {
+  // context
+  const [auth, setAuth] = useAuth();
+  //   state
+  const [username, setUsername] = useState('');
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [company, setCompany] = useState('');
+  const [address, setAddress] = useState('');
+  const [phone, setPhone] = useState('');
+  const [about, setAbout] = useState('');
+  const [loading, setLoading] = useState(false);
+  const [photo, setPhoto] = useState(null);
+  const [uploading, setUploading] = useState(false);
+  // hooks
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (auth.user) {
+      setUsername(auth.user?.username);
+      setName(auth.user?.name);
+      setEmail(auth.user?.email);
+      setCompany(auth.user?.company);
+      setAddress(auth.user?.address);
+      setPhone(auth.user?.phone);
+      setAbout(auth.user?.about);
+      setPhoto(auth.user?.photo);
+    }
+  }, []);
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      console.log({
+        username,
+        name,
+        email,
+        company,
+        address,
+        phone,
+        about,
+        photo,
+      });
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
   return (
     <>
       <h1 className='display-1 bg-primary text-light p-5'>Profile</h1>
       <div className='container-fluid'>
         <Sidebar />
-        <div className='container mt-2'>profile update form</div>
+        <div className='container mt-2'>
+          <div className='row'>
+            <div className='col-lg-8  offset-lg-2 mt-2'>
+              <form onSubmit={handleSubmit}>
+                <input
+                  type='text'
+                  placeholder='Update your username'
+                  className='form-control mb-4'
+                  value={username}
+                  onChange={(e) =>
+                    setUsername(slugify(e.target.value.toLowerCase()))
+                  }
+                />
+                <input
+                  type='text'
+                  placeholder='Enter your name'
+                  className='form-control mb-4'
+                  value={name}
+                  onChange={(e) => setName(e.target.value)}
+                />
+                <input
+                  type='email'
+                  className='form-control mb-4'
+                  value={email}
+                  disabled
+                />
+                <input
+                  type='text'
+                  placeholder='Enter your company name'
+                  className='form-control mb-4'
+                  value={company}
+                  onChange={(e) => setCompany(e.target.value)}
+                />
+                <input
+                  type='text'
+                  placeholder='Enter your address'
+                  className='form-control mb-4'
+                  value={address}
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+                <input
+                  type='text'
+                  placeholder='Enter your phone number'
+                  className='form-control mb-4'
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+                <textarea
+                  placeholder='Describe yourself'
+                  className='form-control mb-4'
+                  value={about}
+                  onChange={(e) => setAbout(e.target.value)}
+                  maxLength={200}
+                />
+                <button
+                  className='btn btn-primary col-12 mb-4'
+                  disabled={loading}
+                >
+                  {loading ? 'Processing' : 'Update Profile'}
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
