9e05fee8a4a6c6ba03a55b99f97ba51a13896e98
Monte <monte6198@gmail.com>
2023-06-07 02:05:03 +0900

    feat: Transfer 및 Request 버튼 추가

--- flutter/toonflix/lib/main.dart
+++ flutter/toonflix/lib/main.dart
@@ -13,7 +13,7 @@ class App extends StatelessWidget {
       home: Scaffold(
         backgroundColor: const Color(0xFF181818),
         body: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 40),
+          padding: const EdgeInsets.symmetric(horizontal: 20),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
@@ -68,10 +68,11 @@ class App extends StatelessWidget {
                 height: 30,
               ),
               Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
                   Container(
                     decoration: BoxDecoration(
-                      color: const Color(0xFFF2B33A),
+                      color: const Color(0xFFF1B33A),
                       borderRadius: BorderRadius.circular(45),
                     ),
                     child: const Padding(
@@ -82,6 +83,25 @@ class App extends StatelessWidget {
                       child: Text(
                         "Transfer",
                         style: TextStyle(
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF1F2123),
+                      borderRadius: BorderRadius.circular(45),
+                    ),
+                    child: const Padding(
+                      padding: EdgeInsets.symmetric(
+                        vertical: 20,
+                        horizontal: 50,
+                      ),
+                      child: Text(
+                        "Request",
+                        style: TextStyle(
+                          color: Colors.white,
                           fontSize: 20,
                         ),
                       ),
