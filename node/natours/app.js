5b55e79698d28311b2f398b89273eb6f30fe7a65
Monte <monte6198@gmail.com>
2023-07-01 15:13:41 +0900

    refactor: (req, res) 함수명 부여

--- node/natours/app.js
+++ node/natours/app.js
@@ -6,33 +6,18 @@ const port = 8080;
 
 app.use(express.json());
 
-// app.get('/', (req, res) => {
-//   res.status(200).json({ message: 'Hello from the server.', app: 'Natours' });
-// });
-
-// app.post('/', (req, res) => {
-//   res.send('You can post to this endpoint.');
-// });
-
 const tours = JSON.parse(
   fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`)
 );
 
-app.get('/api/v1/tours', (req, res) => {
+const getAllTours = (req, res) => {
   res.status(200).json({
     status: 'success',
     results: tours.length,
     data: { tours },
   });
-});
-
-// :variable
-// req.body는 클라이언트 측에서
-// req.params는 API URL에서 (여기서는 :id 가 들어감))
-
-// app.get('/api/v1/tours/:id/:a?/:b', (req, res) => {
-// 위 URL에서 ?는 optional parameter
-app.get('/api/v1/tours/:id', (req, res) => {
+};
+const getTour = (req, res) => {
   console.log(req.params);
   const id = Number(req.params.id);
 
@@ -51,9 +36,8 @@ app.get('/api/v1/tours/:id', (req, res) => {
     // results: tours.length,
     data: { tour },
   });
-});
-
-app.post('/api/v1/tours', (req, res) => {
+};
+const createTour = (req, res) => {
   const newId = tours[tours.length - 1].id + 1;
   const newTour = Object.assign({ id: newId }, req.body);
 
@@ -68,9 +52,8 @@ app.post('/api/v1/tours', (req, res) => {
       });
     }
   );
-});
-
-app.patch('/api/v1/tours/:id', (req, res) => {
+};
+const updateTour = (req, res) => {
   if (req.params.id > tours.length - 1) {
     return res.status(404).json({
       status: 'fail',
@@ -83,9 +66,8 @@ app.patch('/api/v1/tours/:id', (req, res) => {
       tour: `Updated tour #${req.params.id}`,
     },
   });
-});
-
-app.delete('/api/v1/tours/:id', (req, res) => {
+};
+const deleteTour = (req, res) => {
   if (req.params.id > tours.length - 1) {
     return res.status(404).json({
       status: 'fail',
@@ -96,7 +78,18 @@ app.delete('/api/v1/tours/:id', (req, res) => {
     status: 'success',
     data: null,
   });
-});
+};
+
+app.get('/api/v1/tours', getAllTours);
+// :variable
+// req.body는 클라이언트 측에서
+// req.params는 API URL에서 (여기서는 :id 가 들어감))
+// app.get('/api/v1/tours/:id/:a?/:b', (req, res) => {
+// 위 URL에서 ?는 optional parameter
+app.get('/api/v1/tours/:id', getTour);
+app.post('/api/v1/tours', createTour);
+app.patch('/api/v1/tours/:id', updateTour);
+app.delete('/api/v1/tours/:id', deleteTour);
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
