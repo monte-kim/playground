3314d95676b30ad56d9f6759d95de25cc959e799
taehwan01 <monte6198@gmail.com>
2023-07-01 23:31:01 +0900

    feat: env 환경변수 추가

--- node/natours/app.js
+++ node/natours/app.js
@@ -5,12 +5,13 @@ const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
-const port = 8080;
 
 // MIDDLEWARES
-app.use(morgan('dev'));
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(express.json());
-app.use(express.static(`${__dirname}/public`))
+app.use(express.static(`${__dirname}/public`));
 
 // app.use((req, res, next) => {
 //   console.log('Hello from middleware');
--- node/natours/server.js
+++ node/natours/server.js
@@ -1,7 +1,10 @@
+const dotenv = require('dotenv');
+dotenv.config({ path: './config.env' });
+
 const app = require('./app');
 
 // START SERVER
-const port = 8080;
+const port = process.env.PORT || 8080;
 app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
