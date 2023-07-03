6cb1cf0861d157d04990c49aefd9181bf4bd5ec1
Monte <monte6198@gmail.com>
2023-07-03 18:56:51 +0900

    feat: DB 연결 추가

--- node/natours/server.js
+++ node/natours/server.js
@@ -1,5 +1,13 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+
 dotenv.config({ path: './config.env' });
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+mongoose.connect(DB).then(() => console.log('DB connection successful!'));
 
 const app = require('./app');
 
