4b798422145fbd76bf7b4d3d3a590460372a9894
Monte <monte6198@gmail.com>
2023-07-04 18:49:41 +0900

    feat: create tour API 추가

--- node/natours/routes/tourRoutes.js
+++ node/natours/routes/tourRoutes.js
@@ -3,17 +3,16 @@ const tourController = require('../controllers/tourController');
 
 const tourRouter = express.Router();
 
-tourRouter.param('id', tourController.checkID);
+// tourRouter.param('id', tourController.checkID);
 
 tourRouter.get('/', tourController.getAllTours);
 // :variable
 // req.body는 클라이언트 측에서
 // req.params는 API URL에서 (여기서는 :id 가 들어감))
-// app.get('/api/v1/tours/:id/:a?/:b', (req, res) => {
+// app.get('/api/v1/tours/:id/:a?/:b', (req, r es) => {
 // 위 URL에서 ?는 optional parameter
 tourRouter.get('/:id', tourController.getTour);
-// middleware: tourController.checkBody
-tourRouter.post('/', tourController.checkBody, tourController.createTour);
+tourRouter.post('/', tourController.createTour);
 tourRouter.patch('/:id', tourController.updateTour);
 tourRouter.delete('/:id', tourController.deleteTour);
 
