c1c6d9f759c6501a6749b0f4c43984ad9226893c
taehwan01 <monte6198@gmail.com>
2023-08-28 03:12:48 +0900

    feat: document middleware 추가

--- node/natours/models/tourModel.js
+++ node/natours/models/tourModel.js
@@ -1,4 +1,6 @@
 const mongoose = require('mongoose');
+// eslint-disable-next-line import/no-extraneous-dependencies
+const slugify = require('slugify');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -8,6 +10,7 @@ const tourSchema = new mongoose.Schema(
       unique: true,
       trim: true,
     },
+    slug: String,
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -53,6 +56,10 @@ const tourSchema = new mongoose.Schema(
       // select: false,
     },
     startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
   {
     toJSON: { virtuals: true },
@@ -63,6 +70,30 @@ const tourSchema = new mongoose.Schema(
 // 비즈니스 모델과 비즈니스 로직을 최대한 분리하기 위해
 tourSchema.virtual('durationWeeks').get(function () {
   return this.duration / 7;
+});
+
+// DOCUMENT MIDDLEWARE: this = current document
+// runs before '.save()' and '.create()
+// .insertMany()에서는 안 통하고, 방금 저장 및 생성한 데이터와 관련된 함수를 수행
+tourSchema.pre('save', function (next) {
+  this.slug = slugify(this.name, { lower: true });
+  next();
+});
+
+// tourSchema.pre('save', function (next) {
+//   console.log('Will save document');
+//   next();
+// });
+
+// tourSchema.post('save', (doc, next) => {
+//   console.log(doc);
+//   next();
+// });
+
+// QUERY MIDDLEWARE: this = current query
+tourSchema.pre('find', function (next) {
+  this.find({ secretTour: { $ne: true } });
+  next();
 });
 
 const Tour = mongoose.model('Tour', tourSchema);
