61d4f8c2bad4d72ce998deb83f9112b97a9af8c7
Monte <monte6198@gmail.com>
2023-09-04 16:16:07 +0900

    refactor: try catch 문 모듈화

--- node/natours/controllers/tourController.js
+++ node/natours/controllers/tourController.js
@@ -1,5 +1,6 @@
 import Tour from '../models/tourModel.js';
 import APIFeatures from '../utils/apiFeatures.js';
+import catchAsync from '../utils/catchAsync.js';
 // const tours = JSON.parse(
 //   fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
 // );
@@ -13,6 +14,7 @@ import APIFeatures from '../utils/apiFeatures.js';
 //   }
 //   next();
 // };
+
 export default class TourController {
   aliasTopTours = (req, res, next) => {
     req.query.limit = '5';
@@ -21,176 +23,127 @@ export default class TourController {
     next();
   };
 
-  getAllTours = async (req, res) => {
-    try {
-      const features = new APIFeatures(Tour.find(), req.query)
-        .filter()
-        .sort()
-        .limitFields()
-        .paginate();
-      const tours = await features.query; // 이렇게 분리하는 것은 위에서 sorting, limiting 과 같은 다양한 함수를 요구에 따라 추가하기 위함
+  getAllTours = catchAsync(async (req, res, next) => {
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
+    const tours = await features.query; // 이렇게 분리하는 것은 위에서 sorting, limiting 과 같은 다양한 함수를 요구에 따라 추가하기 위함
 
-      res.status(200).json({
-        status: 'success',
-        requestedAt: req.requestTime,
-        results: tours.length,
-        data: { tours },
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
+    res.status(200).json({
+      status: 'success',
+      requestedAt: req.requestTime,
+      results: tours.length,
+      data: { tours },
+    });
+  });
 
-  getTour = async (req, res) => {
-    try {
-      const tour = await Tour.findById(req.params.id);
-      // Tour.findOne({ _id: req.params.id });
+  getTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({ _id: req.params.id });
 
-      res.status(200).json({
-        status: 'success',
-        data: { tour },
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
+    res.status(200).json({
+      status: 'success',
+      data: { tour },
+    });
+  });
 
-  createTour = async (req, res) => {
-    try {
-      // const newTour = new Tour({});
-      // newTour.save();
-      const newTour = await Tour.create(req.body);
+  createTour = catchAsync(async (req, res, next) => {
+    // const newTour = new Tour({});
+    // newTour.save();
+    const newTour = await Tour.create(req.body);
 
-      res.status(201).json({
-        status: 'success',
-        data: { tour: newTour },
-      });
-    } catch (err) {
-      res.status(400).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
+    res.status(201).json({
+      status: 'success',
+      data: { tour: newTour },
+    });
+  });
 
   // patch는 req.body와 DB가 다른 부분만 업데이트 해줌
-  updateTour = async (req, res) => {
-    try {
-      const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
-        new: true,
-        runValidators: true,
-      });
-      res.status(200).json({
-        status: 'success',
-        data: {
-          tour,
-        },
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
+  updateTour = catchAsync(async (req, res, next) => {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour,
+      },
+    });
+  });
 
-  deleteTour = async (req, res) => {
-    try {
-      // 삭제 관련 API에서는 클라이언트에게 다시 전송하는 데이터는 없음
-      await Tour.findByIdAndDelete(req.params.id);
-      res.status(204).json({
-        status: 'success',
-        data: null,
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
+  deleteTour = catchAsync(async (req, res, next) => {
+    // 삭제 관련 API에서는 클라이언트에게 다시 전송하는 데이터는 없음
+    await Tour.findByIdAndDelete(req.params.id);
+    res.status(204).json({
+      status: 'success',
+      data: null,
+    });
+  });
 
-  getTourStats = async (req, res) => {
-    try {
-      const stats = await Tour.aggregate([
-        {
-          $match: { ratingsAverage: { $gte: 4.5 } },
+  getTourStats = catchAsync(async (req, res, next) => {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numberOfTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
         },
-        {
-          $group: {
-            _id: { $toUpper: '$difficulty' },
-            numberOfTours: { $sum: 1 },
-            numRatings: { $sum: '$ratingsQuantity' },
-            avgRating: { $avg: '$ratingsAverage' },
-            avgPrice: { $avg: '$price' },
-            minPrice: { $min: '$price' },
-            maxPrice: { $max: '$price' },
+      },
+      { $sort: { avgPrice: 1 } },
+      { $match: { _id: { $ne: 'EASY' } } },
+    ]);
+
+    res.status(200).json({
+      status: 'success',
+      data: { stats },
+    });
+  });
+
+  getMonthlyPlan = catchAsync(async (req, res, next) => {
+    const year = req.params.year * 1;
+
+    const plan = await Tour.aggregate([
+      {
+        $unwind: '$startDates',
+      },
+      {
+        $match: {
+          startDates: {
+            $gte: new Date(`${year}-01-01`),
+            $lte: new Date(`${year}-12-31`),
           },
         },
-        { $sort: { avgPrice: 1 } },
-        { $match: { _id: { $ne: 'EASY' } } },
-      ]);
-
-      res.status(200).json({
-        status: 'success',
-        data: { stats },
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
-
-  getMonthlyPlan = async (req, res) => {
-    try {
-      const year = req.params.year * 1;
-
-      const plan = await Tour.aggregate([
-        {
-          $unwind: '$startDates',
+      },
+      {
+        $group: {
+          _id: { $month: '$startDates' },
+          numberOfTourStarts: { $sum: 1 },
+          tours: { $push: '$name' },
         },
-        {
-          $match: {
-            startDates: {
-              $gte: new Date(`${year}-01-01`),
-              $lte: new Date(`${year}-12-31`),
-            },
-          },
+      },
+      {
+        $addFields: {
+          month: '$_id',
         },
-        {
-          $group: {
-            _id: { $month: '$startDates' },
-            numberOfTourStarts: { $sum: 1 },
-            tours: { $push: '$name' },
-          },
-        },
-        {
-          $addFields: {
-            month: '$_id',
-          },
-        },
-        { $project: { _id: 0 } },
-        { $sort: { numberOfTourStarts: -1 } },
-        // { $limit: 2 },
-      ]);
+      },
+      { $project: { _id: 0 } },
+      { $sort: { numberOfTourStarts: -1 } },
+      // { $limit: 2 },
+    ]);
 
-      res.status(200).json({
-        status: 'success',
-        data: { plan },
-      });
-    } catch (err) {
-      res.status(404).json({
-        status: 'fail',
-        message: err,
-      });
-    }
-  };
+    res.status(200).json({
+      status: 'success',
+      data: { plan },
+    });
+  });
 }
