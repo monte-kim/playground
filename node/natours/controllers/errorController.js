e99188083c49e371118a9f7e0fb171bc5b22c4eb
Monte <monte6198@gmail.com>
2023-09-04 21:59:26 +0900

    feat: tour API 유효성 에러 핸들링 추가

--- node/natours/controllers/errorController.js
+++ node/natours/controllers/errorController.js
@@ -11,6 +11,11 @@ const handleDuplicateFieldsDB = (err) => {
   const message = `Duplicate field value: ${value}. Please use another value!`;
   return new AppError(message, 400);
 };
+const handleValidationErrorDB = (err) => {
+  const errors = Object.values(err.errors).map((el) => el.message);
+  const message = `Invalid input data. ${errors.join('. ')}`;
+  return new AppError(message, 400);
+};
 
 const sendErrorDev = (err, res) => {
   res.status(err.statusCode).json({
@@ -45,8 +50,11 @@ export default class ErrorController {
     } else if (NODE_ENV === 'production') {
       let error = { ...err };
       console.log(err);
+      // mongoose errors
       if (err.name === 'CastError') error = handleCastErrorDB(error);
       if (err.code === 11000) error = handleDuplicateFieldsDB(error);
+      if (err.name === 'ValidationError')
+        error = handleValidationErrorDB(error);
 
       sendErrorProd(error, res);
     }
