7f21b67ccc4aee18fefb220e453a2c13ef63b857
Monte <monte6198@gmail.com>
2023-09-04 16:39:18 +0900

    refactor: 환경별 에러 출력 변경

--- node/natours/controllers/errorController.js
+++ node/natours/controllers/errorController.js
@@ -1,12 +1,30 @@
+import { NODE_ENV } from '../config.js';
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+const sendErrorProd = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+  });
+};
+
 export default class ErrorController {
   globalErrorHandler = (err, req, res, next) => {
     console.log(err.stack);
     err.statusCode = err.statusCode || 500;
     err.status = err.status || 'error';
 
-    res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
+    if (NODE_ENV === 'development') {
+      sendErrorDev(err, res);
+    } else if (NODE_ENV === 'production') {
+      sendErrorProd(err, res);
+    }
   };
 }
