e1f468b65b0cb4e98dca5bdfa6563d651104e039
taehwan01 <monte6198@gmail.com>
2023-09-13 08:40:12 +0900

    feat: hellow world widget 분리

--- flutter/dice/lib/main.dart
+++ flutter/dice/lib/main.dart
@@ -4,10 +4,39 @@ void main() {
   runApp(
     const MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text("Hello World"),
-        ),
+        body: GradientContainer(),
       ),
     ),
   );
+}
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 255, 60, 0),
+            // Colors.white,
+            Colors.purple,
+          ],
+          begin: Alignment.topLeft,
+          end: Alignment.bottomRight,
+        ),
+      ),
+      child: const Center(
+        child: Text(
+          "Hello, Monte",
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 28,
+          ),
+        ),
+      ),
+    );
+    // throw UnimplementedError();
+  }
 }
