a0a582140a9307235e27328c654dabf379212904
Monte <monte6198@gmail.com>
2023-09-15 16:49:30 +0900

    feat: 퀴즈 데이터 추가

--- flutter/quiz_app/lib/questions_screen.dart
+++ flutter/quiz_app/lib/questions_screen.dart
@@ -12,6 +12,34 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screenoqwiejfoqwrfoqeoiqerjfoißqj');
+    return SizedBox(
+      width: double.infinity,
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          const Text(
+            'Question 1',
+            style: TextStyle(color: Colors.white, fontSize: 28),
+          ),
+          const SizedBox(height: 30),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer'),
+          ),
+          ElevatedButton(
+            onPressed: () {},
+            child: const Text('Answer'),
+          ),
+        ],
+      ),
+    );
   }
 }
