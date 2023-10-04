93ccfef069f077e7bcb6fded52407fa17226abd4
Monte <monte6198@gmail.com>
2023-10-04 16:53:50 +0900

    feat: 레시피 기타 정보 추가

--- flutter/meals/lib/screens/meals.dart
+++ flutter/meals/lib/screens/meals.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:meals/models/meal.dart';
+import 'package:meals/widgets/meal_item.dart';
 
 class MealsScreen extends StatelessWidget {
   const MealsScreen({super.key, required this.title, required this.meals});
@@ -35,7 +36,7 @@ class MealsScreen extends StatelessWidget {
               ),
             )
           : ListView.builder(
-              itemBuilder: (ctx, index) => Text(meals[index].title),
+              itemBuilder: (ctx, index) => MealItem(meal: meals[index]),
               itemCount: meals.length,
             ),
     );
