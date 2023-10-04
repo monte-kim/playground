6cbb9fdea00e9ac40dfab4fec76f251a283e0924
Monte <monte6198@gmail.com>
2023-10-04 13:20:54 +0900

    feat: Categories 추가

--- flutter/meals/lib/main.dart
+++ flutter/meals/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import 'package:google_fonts/google_fonts.dart';
+import 'package:meals/screens/categories.dart';
 
 final theme = ThemeData(
   useMaterial3: true,
@@ -8,7 +9,7 @@ final theme = ThemeData(
     brightness: Brightness.dark,
     seedColor: const Color.fromARGB(255, 131, 57, 0),
   ),
-  textTheme: GoogleFonts.latoTextTheme(),
+  textTheme: GoogleFonts.varelaRoundTextTheme(),
 );
 
 void main() {
@@ -22,7 +23,7 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: theme,
-      home: // Todo ...,
+      home: const CategoriesScreen(),
     );
   }
 }
