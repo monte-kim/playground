d790b996d63e438d028c322e796c1c14812d6ca5
Monte <monte6198@gmail.com>
2023-10-10 17:27:09 +0900

    feat: 백엔드 및 firebase 연결

--- flutter/shopping_list/lib/widgets/grocery_list.dart
+++ flutter/shopping_list/lib/widgets/grocery_list.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/material.dart';
+import 'dart:convert';
 
-// import 'package:shopping_list/data/dummy_items.dart';
+import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'package:shopping_list/data/categories.dart';
 import 'package:shopping_list/models/grocery_item.dart';
 import 'package:shopping_list/widgets/new_item.dart';
 
@@ -12,7 +14,60 @@ class GroceryList extends StatefulWidget {
 }
 
 class _GroceryListState extends State<GroceryList> {
-  final List<GroceryItem> _groceryItems = [];
+  var _groceryItems = [];
+  var _isLoading = true;
+  String _error = '';
+
+  @override
+  void initState() {
+    _loadItems();
+    super.initState();
+  }
+
+  void _loadItems() async {
+    final url = Uri.https(
+        'flutter-prep-959cd-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'shopping-list.json');
+
+    try {
+      final response = await http.get(url);
+
+      if (response.statusCode >= 400) {
+        setState(() {
+          _error = 'Failed to fetch data, please try later.';
+        });
+      }
+
+      if (response.body == 'null') {
+        setState(() {
+          _isLoading = false;
+        });
+
+        return;
+      }
+      final Map<String, dynamic> listData = json.decode(response.body);
+      final List<GroceryItem> loadedItems = [];
+      for (final item in listData.entries) {
+        final category = categories.entries
+            .firstWhere((categoryItem) =>
+                categoryItem.value.name == item.value['category'])
+            .value;
+        loadedItems.add(GroceryItem(
+            id: item.key,
+            name: item.value['name'],
+            quantity: item.value['quantity'],
+            category: category));
+      }
+      setState(() {
+        _groceryItems = loadedItems;
+        _isLoading = false;
+      });
+    } catch (error) {
+      setState(() {
+        _error = 'Something went wrong.\nPlease try later.';
+      });
+    }
+  }
 
   void _addItem() async {
     final newItem = await Navigator.of(context).push<GroceryItem>(
@@ -20,6 +75,7 @@ class _GroceryListState extends State<GroceryList> {
         builder: (ctx) => const NewItem(),
       ),
     );
+
     if (newItem == null) {
       return;
     }
@@ -28,8 +84,76 @@ class _GroceryListState extends State<GroceryList> {
     });
   }
 
+  void _removeItem(GroceryItem item) async {
+    final index = _groceryItems.indexOf(item);
+
+    setState(() {
+      _groceryItems.remove(item);
+    });
+
+    final url = Uri.https(
+        'flutter-prep-959cd-default-rtdb.asia-southeast1.firebasedatabase.app',
+        'shopping-list/${_groceryItems[index].id}.json');
+    final response = await http.delete(url);
+    if (response.statusCode >= 400) {
+      setState(() {
+        _groceryItems.insert(index, item);
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget content = const Center(
+        child: Text(
+      "EMPTY",
+      style: TextStyle(
+        fontSize: 24,
+        fontWeight: FontWeight.bold,
+      ),
+    ));
+
+    if (_isLoading) {
+      content = const Center(
+        child: CircularProgressIndicator(),
+      );
+    }
+
+    if (_groceryItems.isNotEmpty) {
+      content = ListView.builder(
+        itemCount: _groceryItems.length,
+        itemBuilder: (ctx, index) => Dismissible(
+          onDismissed: (direction) {
+            _removeItem(_groceryItems[index]);
+          },
+          key: ValueKey(_groceryItems[index].id),
+          child: ListTile(
+            title: Text(_groceryItems[index].name),
+            leading: Container(
+              width: 24,
+              height: 24,
+              color: _groceryItems[index].category.color,
+            ),
+            trailing: Text(_groceryItems[index].quantity.toString()),
+          ),
+        ),
+      );
+    }
+
+    if (_error != '') {
+      content = Center(
+        child: Text(
+          _error,
+          textAlign: TextAlign.center,
+          style: const TextStyle(
+            fontSize: 24,
+            fontWeight: FontWeight.bold,
+            color: Colors.red,
+          ),
+        ),
+      );
+    }
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
@@ -40,60 +164,7 @@ class _GroceryListState extends State<GroceryList> {
           )
         ],
       ),
-      body: _groceryItems.isEmpty
-          ? const Center(
-              child: Text(
-              "EMPTY",
-              style: TextStyle(
-                fontSize: 24,
-                fontWeight: FontWeight.bold,
-              ),
-            ))
-          : ListView.builder(
-              itemCount: _groceryItems.length,
-              itemBuilder: (ctx, index) => Dismissible(
-                onDismissed: (direction) {
-                  setState(() {
-                    _groceryItems.removeAt(index);
-                  });
-                },
-                key: ValueKey(_groceryItems[index].id),
-                child: ListTile(
-                  title: Text(_groceryItems[index].name),
-                  leading: Container(
-                    width: 24,
-                    height: 24,
-                    color: _groceryItems[index].category.color,
-                  ),
-                  trailing: Text(_groceryItems[index].quantity.toString()),
-                ),
-              ),
-            ),
-      // Padding(
-      //   padding: const EdgeInsets.all(24),
-      //   child: Column(
-      //     // crossAxisAlignment: CrossAxisAlignment.center,
-      //     children: [
-      //       for (final groceryItem in groceryItems)
-      //         Row(
-      //           mainAxisSize: MainAxisSize.max,
-      //           children: [
-      //             Container(
-      //               width: 20,
-      //               height: 20,
-      //               color: groceryItem.category.color,
-      //             ),
-      //             const SizedBox(
-      //               width: 8,
-      //             ),
-      //             Text(groceryItem.name),
-      //             Text(groceryItem.quantity.toString()),
-      //             const SizedBox(height: 40),
-      //           ],
-      //         ),
-      //     ],
-      //   ),
-      // ),
+      body: content,
     );
   }
 }
