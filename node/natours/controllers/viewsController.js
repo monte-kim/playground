488820f5b3df434a17242340586bfd6522c9d32e
taehwan01 <monte6198@gmail.com>
2023-10-20 20:07:24 +0900

    feat: tour overview page

--- node/natours/controllers/viewsController.js
+++ node/natours/controllers/viewsController.js
@@ -1,9 +1,19 @@
+import Tour from '../models/tourModel.js';
+import catchAsync from '../utils/catchAsync.js';
+
 export default class ViewsController {
-  getOverview = (req, res) => {
+  getOverview = catchAsync(async (req, res) => {
+    // 1) get tour data from collection
+    const tours = await Tour.find();
+
+    // 2) build template
+
+    // 3) render that template using tour data from 1)
     res.status(200).render('overview', {
       title: 'All Tours',
+      tours,
     });
-  };
+  });
 
   getTour = (req, res) => {
     res.status(200).render('tour', {
