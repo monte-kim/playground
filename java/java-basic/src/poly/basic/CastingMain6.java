5f138756949a2f0685e0f9f449f22e92d675c518
taehwan01 <monte6198@gmail.com>
2024-01-06 14:10:06 +0900

    다형성1

---
+++ java/java-basic/src/poly/basic/CastingMain6.java
@@ -0,0 +1,20 @@
+package poly.basic;
+
+//다운캐스팅을 자동으로 하지 않는 이유
+public class CastingMain6 {
+    public static void main(String[] args) {
+        Parent parent1 = new Parent();
+        call(parent1);
+        Parent parent2 = new Child();
+        call(parent2);
+    }
+
+    private static void call(Parent parent) {
+        if (parent instanceof Child) {
+            System.out.println("Child 인스턴스 맞음");
+            ((Child) parent).childMethod();
+        } else {
+            System.out.println("Child 인스턴스 아님");
+        }
+    }
+}
