a234eeec3652efbc0f012f42fd8c47311c00595a
taehwan01 <monte6198@gmail.com>
2024-12-12 23:17:38 +0900

    section3

---
+++ spring/servlet/src/main/java/hello/servlet/basic/request/RequestParamServlet.java
@@ -0,0 +1,27 @@
+package hello.servlet.basic.request;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Enumeration;
+
+@WebServlet(name = "RequestParamServlet", urlPatterns = "/request-param")
+public class RequestParamServlet extends HttpServlet {
+
+    @Override
+    protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> System.out.println(paramName + " : " + request.getParameter(paramName)));
+
+        String username = request.getParameter("username");
+        String age = request.getParameter("age");
+        System.out.println("username = " + username);
+        System.out.println("age = " + age);
+
+        response.getWriter().write("OK");
+    }
+}
