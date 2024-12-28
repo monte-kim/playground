cc4e560417e39ef064e8e9b05fc24cd67e38f25d
taehwan01 <monte6198@gmail.com>
2024-12-29 03:26:34 +0900

    section5

---
+++ spring/jdbc/src/main/java/hello/jdbc/service/MemberServiceV3_1.java
@@ -0,0 +1,59 @@
+package hello.jdbc.service;
+
+import hello.jdbc.domain.Member;
+import hello.jdbc.repository.MemberRepositoryV3;
+import java.sql.Connection;
+import java.sql.SQLException;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionStatus;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
+
+/** 트랜잭션 - 트랜잭션 매니저 */
+@Slf4j
+@RequiredArgsConstructor
+public class MemberServiceV3_1 {
+
+  private final PlatformTransactionManager transactionManager;
+  private final MemberRepositoryV3 memberRepository;
+
+  public void accountTransfer(String fromId, String toId, int money) throws SQLException {
+    TransactionStatus status =
+        transactionManager.getTransaction(new DefaultTransactionDefinition());
+    try {
+      // 비즈니스 로직
+      businessLogic(fromId, toId, money);
+      transactionManager.commit(status);
+    } catch (Exception e) {
+      transactionManager.rollback(status);
+      throw new IllegalStateException(e);
+    }
+  }
+
+  private void businessLogic(String fromId, String toId, int money) throws SQLException {
+    Member fromMember = memberRepository.findById(fromId);
+    Member toMember = memberRepository.findById(toId);
+
+    memberRepository.update(fromId, fromMember.getMoney() - money);
+    validation(toMember);
+    memberRepository.update(toId, toMember.getMoney() + money);
+  }
+
+  private void validation(Member toMember) {
+    if (toMember.getId().equals("ex")) {
+      throw new IllegalStateException("이체 중 예외 발생");
+    }
+  }
+
+  private void release(Connection conn) {
+    if (conn != null) {
+      try {
+        conn.setAutoCommit(true); // 커넥션 풀을 고려
+        conn.close();
+      } catch (Exception e) {
+        log.info("error", e);
+      }
+    }
+  }
+}
