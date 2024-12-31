d356536e896e9303e9bd01215b3ff01d1da921a9
taehwan01 <monte6198@gmail.com>
2025-01-01 00:06:50 +0900

    section6,7

--- spring/itemservice-db/src/main/java/hello/itemservice/service/ItemServiceV1.java
+++ spring/itemservice-db/src/main/java/hello/itemservice/service/ItemServiceV1.java
@@ -9,8 +9,10 @@ import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional
 @RequiredArgsConstructor
 public class ItemServiceV1 implements ItemService {
 
