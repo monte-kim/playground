e58b165ad13e208a67dd62ad394b76506a89b651
taehwan01 <monte6198@gmail.com>
2025-02-02 11:26:34 +0900

    DataJPA section4

--- spring/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
+++ spring/data-jpa/src/main/java/study/datajpa/DataJpaApplication.java
@@ -4,10 +4,11 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+//@EnableJpaRepositories(basePackages = "study.datajpa.repository")
 public class DataJpaApplication {
 
-  public static void main(String[] args) {
-    SpringApplication.run(DataJpaApplication.class, args);
-  }
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaApplication.class, args);
+    }
 
 }
--- spring/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
+++ spring/data-jpa/src/main/java/study/datajpa/repository/MemberJpaRepository.java
@@ -1,6 +1,8 @@
 package study.datajpa.repository;
 
 import jakarta.persistence.EntityManager;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 import study.datajpa.entity.Member;
@@ -16,9 +18,24 @@ public class MemberJpaRepository {
         return member;
     }
 
+    public void delete(Member member) {
+        em.remove(member);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public Optional<Member> findById(Long id) {
+        Member member = em.find(Member.class, id);
+        return Optional.ofNullable(member);
+    }
+
+    public long count() {
+        return em.createQuery("select count(m) from Member m", Long.class).getSingleResult();
+    }
+
     public Member find(Long id) {
         return em.find(Member.class, id);
     }
-
-
 }
